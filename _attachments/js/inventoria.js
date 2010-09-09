14fb87ee9381a523ca24717d734a0846a5fd6908
Michel Legnered <michel.legnered@gmail.com>
2010-09-09 20:59:47 +0200

    more work on user login and some more code refactoring

--- _attachments/js/inventoria.js
+++ _attachments/js/inventoria.js
@@ -1,22 +1,5 @@
 $(document).ready(function(){
 
-	// Append new inputs
-	$('.value').live('focus', function () {
-		// Check if we are editing the last input field.
-		// If not, we do not want to add another field.
-		if ($(this).parent().next().hasClass('last'))
-			addFormField();
-	});
-					   
-	// Set key (name="") from previous input
-	$('.value').live('focus', function () {
-		value = $(this).prev().val();
-		$(this).attr('name', value);
-	});
-
-	// Set non optional keys to read-only
-	$('.readonly').attr('readonly', true);
-
 	// Upload and retrieve image data
 	/*
 	  See:
@@ -57,9 +40,13 @@ $(document).ready(function(){
 		return false;
 	});
 	*/
-	
-	// Submit form data to DB
-	$('#addInventory').submit(function () {
+
+	/*********************************************************
+	 * Item Submit
+	 ********************************************************/
+
+	// Item submit form
+	$('#addItem').submit(function () {
 
 		var data = prepJSON($(this).serializeArray());
 		$('input').attr('disabled', true);
@@ -71,29 +58,105 @@ $(document).ready(function(){
 		return false;
 	});
 
-	$('#login').submit(function () {
+	// Append new inputs
+	$('.value').live('focus', function () {
+		// Check if we are editing the last input field.
+		// If not, we do not want to add another field.
+		if ($(this).parent().next().hasClass('last'))
+			addFormField();
+	});
+					   
+	// Set key (name="") from previous input
+	$('.value').live('focus', function () {
+		value = $(this).prev().val();
+		$(this).attr('name', value);
+	});
 
-		var data = prepJSON($(this).serializeArray());
-		$.couch.login({
-			"name": data.name,
-			"password": data.password,
-			success: function (resp) { handleLogin(resp) }
-		});
-		
+	// Set non optional keys to read-only
+	$('.readonly').attr('readonly', true);
+
+
+	
+	/*********************************************************
+	 * User login
+	 ********************************************************/
+	
+	// Login form
+	$('#login').submit(function () {
+		user.login(prepJSON($(this).serializeArray()));
 		return false;
 	});
 
+	// Show control bar instead of login form if logged in
+	if (user.name)
+		user.showControlBar();
+	else
+		user.showLoginForm();
+
+	$('.button.logout').click(function () { user.logout() });
+	
 });
+
+var cookie = new Cookie();
+var user = new User();
+
+function User () {
+
+	that = this;
+
+	this.name = cookie.read('username');
+
+	this.login = function (data) {
+		$('input').attr('disabled', true);
+		$('.button.login').val('Loggar in...');
+		$.couch.login({
+			"name": data.name,
+			"password": data.password,
+			success: function (resp) { handleLogin(); }
+		});
+	}
+
+	this.logout = function () {
+		$.couch.logout();
+		cookie.erase('username');
+		this.showLoginForm();
+	}
+
+	this.showControlBar = function () {
+		this.name = cookie.read('username');
+		$('#login').hide();
+		$('#controlBar .username').html(this.name);
+		$('#controlBar').show();		
+	}
+
+	this.showLoginForm = function () {
+		$('input').attr('disabled', false);
+		$('.button.login').val('Logga in');
+		$('#controlBar').hide();
+		$('#login').show();
+	}
+
+	var handleLogin = function () {
+ 		$.couch.session({
+			success: function (res) {
+				cookie.create('username', res.userCtx.name);
+				that.showControlBar();
+			}
+		});
+	}
+}
 
 var first_time = 0;
 
-function itemSaved (resp) {
+var itemSaved = function (resp) {
+
 	console.log(resp);
+
 	btn_goto_item = '<button class="button gotoItem" type="button">Gå till föremål</button>';
 	btn_new_item = '<button class="button newItem" type="button">Nytt föremål</button>';
 
-	$('#addInventory').append(btn_new_item);
-	$('#addInventory').append(btn_goto_item);
+	$('#addItem').append(btn_new_item);
+	$('#addItem').append(btn_goto_item);
 
 	$('.button.newItem').live('click', function () {
 		$('input').attr('disabled', false);
@@ -108,7 +171,7 @@ function itemSaved (resp) {
 	});
 }
 
-function addFormField () {
+var addFormField = function () {
 
 	if (!first_time) {
 		message = message('new_field');
@@ -122,11 +185,7 @@ function addFormField () {
 	$(new_input).insertBefore('#submitInventory');
 }
 
-function handleLogin (response) {
-	console.log(response);
-}
-
-function prepJSON (fields) {
+var prepJSON = function (fields) {
 
 	if (fields.length) {
 		var data = '{';
@@ -142,12 +201,41 @@ function prepJSON (fields) {
 		return {};
 }
 
-function message (message) {
+var message = function (message) {
 	var tag = '<span class="message new_field">'
 	var detag = '</span>';
 	
 	switch (message) {
 	case 'new_field':
 		return tag+'For varje informationsrad du fyller i laggs automatiskt ett nytt falt till -- fardigt att redigera!</span>'+detag;
+	}
+}
+
+// Based on: http://www.quirksmode.org/js/cookies.html
+function Cookie () {
+
+	this.create = function (name,value,days) {
+		if (days) {
+			var date = new Date();
+			date.setTime(date.getTime()+(days*24*60*60*1000));
+			var expires = "; expires="+date.toGMTString();
+		}
+		else var expires = "";
+		document.cookie = name+"="+value+expires+"; path=/";
+	}
+
+	this.read = function (name) {
+		var nameEQ = name + "=";
+		var ca = document.cookie.split(';');
+		for(var i=0;i < ca.length;i++) {
+			var c = ca[i];
+			while (c.charAt(0)==' ') c = c.substring(1,c.length);
+			if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+		}
+		return null;
+	}
+
+	this.erase = function (name) {
+		this.create(name,"",-1);
 	}
 }
